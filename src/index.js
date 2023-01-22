333860dacfecf083b5026c70170a18a1550fdceb
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-01-22 02:41:20 +0200

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const map = {
+    close: bracketsConfig.reduce((acc, it) => {
+      acc[it[1]] = it[0]
+      return acc
+    }, {}),
+    open: bracketsConfig.reduce((acc, it) => {
+      acc[it[0]] = it[1]
+      return acc
+    }, {}),
+  };
+  const openedBrackets = Object.values(map.close);
+  
+  const stack = [];
+  
+  for(let i = 0; i < str.length; i++) {
+    const isOpenBracket = openedBrackets.includes(str[i]);
+    if (isOpenBracket && stack[stack.length - 1] !== map.open[str[i]]) {
+      stack.push(str[i]);
+    } else if (stack[stack.length - 1] === map.close[str[i]]) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
 }
